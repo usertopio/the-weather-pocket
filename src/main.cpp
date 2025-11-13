24524f563571f292c81e0fa7fc1bbfd66f697db1
usertopio <tammasorn.dev@gmail.com>
2025-11-13 23:47:45 +0700

    feat(bme280): ✨ add the weather measurement feature
    Weather measurement feature includes:
    1. Temperature
    2. Humidity
    3. Pressure
    4. Altitude (calculated from sea level pressure constant)
    
    Test the BME280
    Fixes #3

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,53 @@
+// Libraries
+// Arduino framework
 #include <Arduino.h>
 
-const int ledPin = 8; 
+// BME280
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+
+// Define component connection pins
+// BME280
+#define BME_SCK  18 //SCL
+#define BME_MISO 19 //SDA
+#define BME_MOSI 23 //SDO
+#define BME_CS   5 //CSB
+
+#define SEA_LEVEL_PRESSURE_HPA 1010
+
+Adafruit_BME280 bme(BME_CS, BME_MOSI, BME_MISO, BME_SCK);
 
 void setup() {
   Serial.begin(115200);
-  pinMode(ledPin, OUTPUT);
+  delay(1000);
+
+  // BME280
+  Serial.println("Initializing BME280 via SPI...");
+  if (!bme.begin()) {
+    Serial.println("Could not find BME280! Check wiring.");
+    while (1);
+  }
+  Serial.println("BME280 initialized successfully!");
 }
 
 void loop() {
-  digitalWrite(ledPin, HIGH); 
-  Serial.println("LED OFF");
-  delay(1000);                     
-  digitalWrite(ledPin, LOW); 
-  Serial.println("LED ON");  
-  delay(1000);                     
+  // BME280
+  Serial.print("Temperature = ");
+  Serial.print(bme.readTemperature());
+  Serial.println(" °C");
+
+  Serial.print("Humidity = ");
+  Serial.print(bme.readHumidity());
+  Serial.println(" %");
+
+  Serial.print("Pressure = ");
+  Serial.print(bme.readPressure() / 100.0F);
+  Serial.println(" hPa");
+
+  Serial.print("Altitude = ");
+  Serial.print(bme.readAltitude(SEA_LEVEL_PRESSURE_HPA));
+  Serial.println(" m");
+
+  Serial.println();
+  delay(2000);
 }
