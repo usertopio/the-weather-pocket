ce6e3ecb625bed54679386f6f4ea68bd5e239c9d
usertopio <tammasorn.dev@gmail.com>
2025-11-19 21:23:45 +0700

    refactor(oled): ♻️ align the OLED Display constructor from horizontal to vertical

--- src/oledDisplay.cpp
+++ src/oledDisplay.cpp
@@ -1,6 +1,10 @@
 #include <oledDisplay.h>
 
-OLEDDisplay::OLEDDisplay(int screen_width, int screen_height, int address): SCREEN_WIDTH(screen_width), SCREEN_HEIGHT(screen_height), ADDRESS(address), display(screen_width, screen_height, &Wire, -1){}
+OLEDDisplay::OLEDDisplay(int screen_width, int screen_height, int address): 
+    SCREEN_WIDTH(screen_width), 
+    SCREEN_HEIGHT(screen_height), 
+    ADDRESS(address), 
+    display(screen_width, screen_height, &Wire, -1){}
 
 void OLEDDisplay::begin(){
     display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
