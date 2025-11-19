a06640fcfcb23d3f384b1c438cb65c7ad58fc8c2
usertopio <tammasorn.dev@gmail.com>
2025-11-19 20:38:22 +0700

    feat(dht22): ✨ add dht22 using OOP by seperateng header file and  source file

---
+++ include/sensor.h
@@ -0,0 +1,9 @@
+#ifndef SENSOR_H
+#define SENSOR_H
+
+class Sensor {
+    public:
+        virtual void begin() = 0;
+};
+
+#endif
---
+++ src/dht22.cpp
@@ -0,0 +1,3 @@
+#include <dht22.h>
+
+DHT22::DHT22(int dht22_pin): DHT22_PIN(dht22_pin) {}
