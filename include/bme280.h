069b2aefa16f8b7acab412e8ac5f4b15350b41cd
usertopio <tammasorn.dev@gmail.com>
2025-11-19 15:14:35 +0700

    chore(integration): 🔧  clean the integrated code to restart the project

--- include/bme280.h
+++ include/bme280.h
@@ -2,19 +2,21 @@
 #define BME280_H
 
 // Libraries
-// BME280
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
 
-// Define component connection pins
-// BME280
+// Define variables
+// Pinouts
 #define BME_SCK  18 //SCL
 #define BME_MISO 19 //SDA
 #define BME_MOSI 23 //SDO
 #define BME_CS   5 //CSB
 
+// Data
 #define SEA_LEVEL_PRESSURE_HPA 1010
 
+// Declare variables
+// Sensor object
 extern Adafruit_BME280 bme;
 
 #endif
--- src/bme280.cpp
+++ src/bme280.cpp
@@ -1,4 +1,5 @@
 #include "bme280.h"
 
-// BME280
+// Define variables
+// Sensor object
 Adafruit_BME280 bme(BME_CS, BME_MOSI, BME_MISO, BME_SCK);
