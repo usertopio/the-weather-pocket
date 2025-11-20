591a7bf7a7a8073d9aab7bebe746d7292a0bddbf
usertopio <tammasorn.dev@gmail.com>
2025-11-20 14:56:29 +0700

    refactor(bme280): ♻️ refactor and reorder code in source file

--- src/bme280.cpp
+++ src/bme280.cpp
@@ -2,15 +2,22 @@
 #include <bme280.h>
 
 // Constructor
-BME280::BME280(int bme_mosi, int bme_miso, int bme_scl, int bme_cs):
+BME280::BME280(int bme_mosi, int bme_miso, int bme_sck, int bme_cs) :
+    // Pins
     BME_MOSI(bme_mosi),
     BME_MISO(bme_miso),
-    BME_SCK(bme_scl),
+    BME_SCK(bme_sck),
     BME_CS(bme_cs),
 
-    SEA_LEVEL_PRESSURE_HPA(1010),
+    // State
+    state(State::INIT),
 
-    bme(bme_cs, bme_mosi, bme_miso, bme_scl) {}
+    // Library
+    bme(bme_cs, bme_mosi, bme_miso, bme_sck),
+
+    // Data
+    SEA_LEVEL_PRESSURE_HPA(1010)
+{}
 
  // Initialization
 void BME280::begin(){
