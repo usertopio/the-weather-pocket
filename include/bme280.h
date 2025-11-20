6ea376f60dabf4f471df278b2e5080cc66ec4edf
usertopio <tammasorn.dev@gmail.com>
2025-11-20 14:33:32 +0700

    refactor(bme280): ♻️ restructure the bme280.h

--- include/bme280.h
+++ include/bme280.h
@@ -9,18 +9,47 @@
 #include <sensor.h>
 
 class BME280 : public Sensor {
+    // Constructor
+    public:
+        // Methods
+        BME280(int bme_mosi, int bme_miso, int bme_scl, int bme_cs);
+    
+    // Initialization
+    public:
+        // Methods
+        void begin() override;
+
+    // Pins
     private:
+        // Variables
         const int BME_MOSI; // SDA
         const int BME_MISO; // SDO
         const int BME_SCK; // SCL
         const int BME_CS; // CSB
 
-        const int SEA_LEVEL_PRESSURE_HPA;
+    // State
+    protected:
+        // Variables
+        enum class State {
+            INIT,
+            MEASURE_TEMPERATURE,
+            MEASURE_HUMIDITY,
+            MEASURE_PRESSURE,
+            CALCULATE_ALTITUDE
+        };
+  
+        State state;
 
+        // Methods
+        State getState() const;
+
+    // Data
+    private:
+        // Variables
+        // Library constants
         Adafruit_BME280 bme;
-    public:
-        BME280(int bme_mosi, int bme_miso, int bme_scl, int bme_cs);
-        void begin() override;
+        // My data
+        const int SEA_LEVEL_PRESSURE_HPA;
     };
 
 #endif
--- src/bme280.cpp
+++ src/bme280.cpp
@@ -1,5 +1,7 @@
+// Header files
 #include <bme280.h>
 
+// Constructor
 BME280::BME280(int bme_mosi, int bme_miso, int bme_scl, int bme_cs):
     BME_MOSI(bme_mosi),
     BME_MISO(bme_miso),
@@ -10,6 +12,7 @@ BME280::BME280(int bme_mosi, int bme_miso, int bme_scl, int bme_cs):
 
     bme(bme_cs, bme_mosi, bme_miso, bme_scl) {}
 
+ // Initialization
 void BME280::begin(){
     bme.begin();
 }
